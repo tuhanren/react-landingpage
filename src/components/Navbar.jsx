02e44b4df524bce9da29a242868f5282abbf456b
TuHanren <tu.hanren@gmail.com>
2022-06-02 22:01:33 -0400

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
-    <div className="w-full h-[90px]">
-      <div className="max-w-[1240px] mx-auto px-4 flex justify-between">
-        <div>DEFI</div>
+    <div className="w-full h-[90px] bg-black">
+      <div className="max-w-[1240px] mx-auto px-4 flex justify-between items-center h-full">
         <div>
-          <ul>
+          <h1 className="text-[#00d8ff]">DEFI</h1>
+        </div>
+        <div className="hidden md:flex">
+          <ul className="flex text-white items-center">
             <li>Platform</li>
             <li>Developers</li>
             <li>Community</li>
             <li>About</li>
           </ul>
-          <button>Use Defi</button>
+          <button className="ml-4">Use Defi</button>
+        </div>
+        {/* Hamburger menu */}
+        <div onClick={handleNav} className="block md:hidden">
+          {nav ? (
+            <AiOutlineClose size={30} className="text-white" />
+          ) : (
+            <AiOutlineMenu size={30} className="text-white" />
+          )}
+        </div>
+        {/* Mobile Menu */}
+        <div
+          className={
+            nav
+              ? "w-full bg-black text-white absolute top-[90px] left-0 flex justify-center text-center"
+              : "absolute left-[-100%]"
+          }
+        >
+          <ul>
+            <li className="text-2xl">Platform</li>
+            <li className="text-2xl">Developers</li>
+            <li className="text-2xl">Community</li>
+            <li className="text-2xl">About</li>
+            <button className="m-8">Use Defi</button>
+          </ul>
         </div>
       </div>
     </div>
